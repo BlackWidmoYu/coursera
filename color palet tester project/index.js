a17e1f7f68474389e1e5b91b35070fb32f7f428c
BlackWidmoYu <filipdominiak00@gmail.com>
2021-07-04 18:29:36 +0200

    coraz lepiej

--- color palet tester project/index.js
+++ color palet tester project/index.js
@@ -6,9 +6,18 @@ function colorChange(){
     let value1 = parseInt(inp[0].value,10);
     let value2 = parseInt(inp[1].value,10);
     let value3 = parseInt(inp[2].value,10);
-
-
     tester[0].style.backgroundColor = "#"+ value1.toString(16) + value2.toString(16) + value3.toString(16) ; 
-   
+}
+function colorChangeRange(){
+   // console.log(inp[3].value,inp[4].value,inp[5].value);
+    let value1 = parseInt(inp[3].value,10);
+    let value2 = parseInt(inp[4].value,10);
+    let value3 = parseInt(inp[5].value,10);
+    tester[0].style.backgroundColor = "#"+ value1.toString(16) + value2.toString(16) + value3.toString(16) ; 
 }
 document.querySelector("button").addEventListener('click',colorChange);
+document.querySelectorAll(".range").forEach(()=>{
+    this.addEventListener('change',colorChangeRange);
+    this.addEventListener('input',colorChangeRange);
+   
+}) ;
